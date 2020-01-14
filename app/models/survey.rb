a7e9a335e58cc0aab0dc9f55dd08701f52da683b
Student <example@example.com>
2020-01-14 11:28:11 -0800

    add callback titleize

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,11 @@ class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
   validates :title, presence: true
   validates_length_of :title, maximum: 100
+  before_save(:titleize_survey)
+
+  private
+  def titleize_survey
+    self.title = self.title.titleize
+  end
+  
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -4,4 +4,8 @@ describe Survey do
   it { should have_many(:questions)}
   it { should validate_presence_of :title }
   it { should validate_length_of(:title).is_at_most(100) }
+  it("titleizes the name of a survey") do
+    survey = Survey.create({title: "giant steps"})
+    expect(survey.title()).to(eq("Giant Steps"))
+  end
 end
