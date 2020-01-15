86857269a964dc7e6822783edba02f84c56b4cec
Student <example@example.com>
2020-01-14 16:11:56 -0800

    Chuck Norris can Code

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
-
+# seeds database with fake information
+gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# survey_list = [
+#   [ "John Oak", "Quercus" ],
+#   [ "Jane Pine", "Pinus" ],
+#   [ "Sid Sycamore", "Platanus" ],
+#   [ "Allister Alder", "Alnus" ],
+#   [ "Betty Birch", "Betula"],
+#   [ "Candy Cherry", "Prunus"]
+# ]
+#
+# survey_list.each do |author, title|
+#   Survey.create( author: author, title: title )
+# end
+Survey.destroy_all
+Question.destroy_all
+
+50.times do |index|
+  surveys = []
+  surveys.push(Survey.create!(title: Faker::TvShows::BreakingBad.episode, author: Faker::Superhero.name))
+  surveys
+  surveys.each do |survey|
+  Question.create! :q1 => Faker::ChuckNorris.fact,
+                   :survey_id => survey.id
+  end
+end
+
+p "Created #{Survey.count} surveys"
+# p "#{surveys}"
